463e9ff7d149250a197f3d3e0ce6005695f3359e
BiramMbodj <bremsmbodj@gmail.com>
2023-03-05 20:29:51 +0000

    Create new activity 'ListActivity'

--- androidcvclient/app/src/main/java/sn/esmt/cvclientapp/MainActivity.java
+++ androidcvclient/app/src/main/java/sn/esmt/cvclientapp/MainActivity.java
@@ -2,13 +2,26 @@ package sn.esmt.cvclientapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button consulter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.consulter.findViewById(R.id.consulter);
+
+        consulter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent nouvelle_activite = new Intent(getApplicationContext(), )
+            }
+        });
     }
 }
