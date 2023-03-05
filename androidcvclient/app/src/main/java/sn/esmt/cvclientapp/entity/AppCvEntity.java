3750f86b093e141e76dde9a9616ce8d1420a4a69
BiramMbodj <bremsmbodj@gmail.com>
2023-03-05 00:02:14 +0000

    Add new package entity and paste AppCvEntity inside entity

---
+++ androidcvclient/app/src/main/java/sn/esmt/cvclientapp/entity/AppCvEntity.java
@@ -0,0 +1,14 @@
+package sn.esmt.cvclientapp.entity;
+
+public class AppCvEntity {
+
+    private Integer id;
+    private String first_name;
+    private String last_name;
+    private Integer age;
+    private String address;
+    private String email;
+    private String specialite;
+    private String niveauEtude;
+    private String exp_pro;
+}
