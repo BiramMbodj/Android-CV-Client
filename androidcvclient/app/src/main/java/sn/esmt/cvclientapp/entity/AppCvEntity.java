6d214e61cd18432c882cdc894eb1227cf9534b48
BiramMbodj <bremsmbodj@gmail.com>
2023-03-05 00:11:05 +0000

    Edit class 'AppCvEntity'

--- androidcvclient/app/src/main/java/sn/esmt/cvclientapp/entity/AppCvEntity.java
+++ androidcvclient/app/src/main/java/sn/esmt/cvclientapp/entity/AppCvEntity.java
@@ -11,4 +11,80 @@ public class AppCvEntity {
     private String specialite;
     private String niveauEtude;
     private String exp_pro;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSpecialite() {
+        return specialite;
+    }
+
+    public void setSpecialite(String specialite) {
+        this.specialite = specialite;
+    }
+
+    public String getNiveauEtude() {
+        return niveauEtude;
+    }
+
+    public void setNiveauEtude(String niveauEtude) {
+        this.niveauEtude = niveauEtude;
+    }
+
+    public String getExp_pro() {
+        return exp_pro;
+    }
+
+    public void setExp_pro(String exp_pro) {
+        this.exp_pro = exp_pro;
+    }
+
+
+
+
 }
